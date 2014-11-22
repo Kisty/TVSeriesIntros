cd924a3fd359a9113f65e15b089c553bf8ea40ff
Timothy Kist <zboarda@gmail.com>
2014-11-22 01:22:33 +0000

    WIP: two ronnies intro with windows support

--- PoC/src/com/sceneclipper/officeIntros/TVIntros.java
+++ PoC/src/com/sceneclipper/officeIntros/TVIntros.java
@@ -14,30 +14,31 @@ import com.musicg.fingerprint.FingerprintSimilarity;
 import com.musicg.fingerprint.FingerprintSimilarityComputer;
 import com.musicg.wave.Wave;
 
-public class OfficeIntros {
+public class TVIntros {
 	public static void main(String[] args) throws IOException, InterruptedException {
-		String officeTheme = "theoffice.wav";
+		String theme = "two_ronnies.wav";
 		String outputFolder = "Intros";
+
+		final String converterProgram = "ffmpeg";
 		
 		ArrayList<String> intros = new ArrayList<String>();
 				
-		File introDir = new File("/home/tom/Development/theoffice/" + outputFolder);
+		File introDir = new File("\"C:\\Users\\Tim\\Videos\\Two Ronnies\\" + outputFolder + "\"");
 		if(!introDir.exists()) {
 			introDir.mkdir();
 		}
 
 		//TODO whole directory at a time
-		Files.walk(Paths.get("/home/tom/Development/theoffice")).forEach(filePath -> {
+		Files.walk(Paths.get("C:\\Users\\Tim\\Videos\\Two Ronnies")).forEach(filePath -> {
 			
-			if (Files.isRegularFile(filePath) && filePath.toString().endsWith(".mkv")) {
+			if (Files.isRegularFile(filePath) && filePath.toString().endsWith(".mpg")) {
 				intros.add(filePath.toString());
 				
-				//String officeEpisode = "/home/tom/Development/theoffice/[5.26]_Company_Picnic.avi";
-				String officeEpisode = filePath.toString();
-				
-				String officeEpisodeSound = officeEpisode.substring(0, officeEpisode.lastIndexOf(".")) + "-sound.wav";
-				
-				String transcodeCommand = "avconv -i " + officeEpisode + " -ss 0 -t 500 -acodec pcm_s16le -ac 1 -ar 16000 " + officeEpisodeSound;
+				//String episode = "/home/tom/Development/theoffice/[5.26]_Company_Picnic.avi";
+				String episode = filePath.toString();
+
+				String episodeSound = episode.substring(0, episode.lastIndexOf(".")) + "-sound.wav";
+				String transcodeCommand = converterProgram + " -i \"" + episode + "\" -ss 0 -t 500 -acodec pcm_s16le -ac 1 -ar 16000 \"" + episodeSound + "\"";
 				System.out.println(transcodeCommand);
 				
 				ProcessBuilder pb = new ProcessBuilder(Arrays.asList(transcodeCommand.split(" ")));
@@ -63,8 +64,8 @@ public class OfficeIntros {
 					e.printStackTrace();
 				}
 								
-				Wave episodeWave = new Wave(officeEpisodeSound);
-				Wave themeWave = new Wave(officeTheme);
+				Wave episodeWave = new Wave(episodeSound);
+				Wave themeWave = new Wave(theme);
 
 				FingerprintManager fpm = new FingerprintManager();
 				byte themeFingerprint[] = fpm.extractFingerprint(themeWave);
@@ -77,12 +78,12 @@ public class OfficeIntros {
 				System.out.println("Match found at " + timePosition);
 				
 				String endTime = String.valueOf(timePosition);
-						
-				transcodeCommand = "avconv -i " + officeEpisode + " -c:v libx264 -s hd1080 -c:a ac3 -ss 0 -t " + endTime + " "
-						+ officeEpisode.substring(0, officeEpisode.lastIndexOf(File.separator)) 
+
+				transcodeCommand = converterProgram + " -i \"" + episode + "\" -c:v libx264 -s hd1080 -c:a ac3 -ss 0 -t " + endTime + " \""
+						+ episode.substring(0, episode.lastIndexOf(File.separator))
 						+ File.separator + outputFolder + File.separator
-						+ officeEpisode.substring(officeEpisode.lastIndexOf(File.separator), officeEpisode.lastIndexOf(".")) + "-intro"
-						+ officeEpisode.substring(officeEpisode.lastIndexOf("."), officeEpisode.length());
+						+ episode.substring(episode.lastIndexOf(File.separator), episode.lastIndexOf(".")) + "-intro"
+						+ episode.substring(episode.lastIndexOf("."), episode.length())+ "\"";
 
 				System.out.println(transcodeCommand);
 				
@@ -107,9 +108,10 @@ public class OfficeIntros {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
+
+				final String removeProgram = "del";
 				try {
-					p = new ProcessBuilder("rm", officeEpisodeSound).start();
+					p = new ProcessBuilder(removeProgram, episodeSound).start();
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -118,6 +120,12 @@ public class OfficeIntros {
 			
 			
 		});
+
+		//If there's only one intro
+		//then finish
+//		if (intros.size() <= 1) {
+//			return;
+//		}
 		
 		String concat = "\"";
 		for(int i = 0; i < intros.size(); i++) {
@@ -127,7 +135,7 @@ public class OfficeIntros {
 		concat = concat.substring(0, concat.lastIndexOf("|"));
 		concat += "\"";
 		
-		String joinIntros = "avconv -i concat:" + concat + " -c:v libx264 -c:a ac3 Intros.avi";
+		String joinIntros = converterProgram + " -i concat:" + concat + " -c:v libx264 -c:a ac3 Intros.avi";
 		
 		System.out.println(joinIntros);
 		
